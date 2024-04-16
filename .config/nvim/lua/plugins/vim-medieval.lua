057fb4e6c609240eeec303326f028c4cc4a92a1a
Jojo <jonatashashimoto@gmail.com>
2024-04-15 22:27:13 -0400

    plugins update

---
+++ .config/nvim/lua/plugins/vim-medieval.lua
@@ -0,0 +1,9 @@
+return {
+  'gpanders/vim-medieval',
+  config = function()
+    vim.cmd [[
+   let g:medieval_langs = ['python=python3', 'ruby', 'sh', 'console=bash', 'javascript=node']
+   ]]
+    vim.api.nvim_set_keymap('n', '<leader><cr>', ':EvalBlock<cr>', {})
+  end
+}
