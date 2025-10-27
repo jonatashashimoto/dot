f2586182adf02aeb62086ebd5f84eec99e6f58b1
Jojo <jonatashashimoto@gmail.com>
2025-10-27 17:18:56 -0300

    add open repository url

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -49,7 +49,11 @@ return {
         -- Mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
         local bufopts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+        -- vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gD', function()
+          vim.cmd('vsplit')
+          vim.lsp.buf.definition()
+        end, { desc = 'Go to Definition (vertical split)' })
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
         -- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
         vim.keymap.set("n", "gi", vim.lsp.buf.incoming_calls, bufopts)
