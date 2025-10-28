eb3040b6900a8dbc103a05cadb6757e87a430e29
Jojo <jonatashashimoto@gmail.com>
2025-10-28 11:18:58 -0300

    fix wit.nvim plugin name

--- .config/nvim/lua/plugins/wit-open-in-browser.lua
+++ .config/nvim/lua/plugins/wit-open-in-browser.lua
@@ -2,6 +2,6 @@ return {
   "aliqyan-21/wit.nvim",
   config = function()
     require('wit').setup()
-    vim.api.nvim_set_keymap('v', 'o', ':WitSearchVisual<cr>', {})
+    vim.api.nvim_set_keymap('v', '<leader>o', ':WitSearchVisual<cr>', {})
   end
 }
